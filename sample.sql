1e33575cdace9e23c682391df89b8a3f458de98a
P Raviteja <patnamraviteja44@gmail.com>
2025-11-19 14:29:34 +0530

    Test PR creation with reviewers

--- sample.sql
+++ sample.sql
@@ -5,4 +5,4 @@ CREATE TABLE test_users (
 );
 
 INSERT INTO test_users VALUES (1, 'John Doe', 'john@example.com');
-INSERT INTO test_users VALUES (2, 'Jane Smith', 'jane@example.com');
+INSERT INTO test_users VALUES (2, 'Jane Smith', 'jane@example.com');"-- New comment" 
